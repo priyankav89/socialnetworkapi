69c4a34d718264976ed55f541136933a0c8a50b8
priyanka vivek <priyaneonlove89@gmail.com>
2023-08-01 03:41:28 -0700

    readme edited

--- README.md
+++ README.md
@@ -18,13 +18,50 @@ When the user test API POST and DELETE routes in Insomnia,Then the user is able 
 
 ## SCREENSHOTS
 
+1. GET all users
+![screenshot](./assets/images/get%20all%20users.png)
+
+2. GET all thoughts
+![screenshot](./assets/images/get%20all%20thoughts.png)
+
+3. GET user by id
+![screenshot](./assets/images/get%20user%20by%20id.png)
+
+4. GET thoughts by id
+![screenshot](./assets/images/get%20thoughts%20by%20id.png)
+
+5. POST user 
+![screenshot](./assets/images/post%20user.png)
+
+6. DELETE user
+![screenshot](./assets/images/delete%20.png)
+
+7. POST add friend 
+![screenshot](./assets/images/post%20add%20friend.png)
+
+8. DELETE friend
+![screenshot](./assets/images/delete%20friend.png)
+
 
 ## WALKTHROUGH VIDEO
 
 
 ## TESTING
 
+1. npm install or npm i to install node package and package.json .
+2. npm run seed to seed data into the database.
+3. npm start to start the application.
+4. Insomnia is used to test the api routes.
+
+## CREDITS
+
+# REFERENCES :
+
+https://coding-boot-camp.github.io/full-stack/mongodb/how-to-install-mongodb
+
+https://mongoosejs.com/docs/models.html
+
 
 ## LICENSE
 
-
+[![License](https://img.shields.io/badge/License-MIT-blue.svg)](https://opensource.org/licenses/MIT)
--- utils/seed.js
+++ utils/seed.js
@@ -1,48 +1,71 @@
+const { fail } = require('assert');
 const connection = require('../config/connection');
-const { User, Thought,Reaction } = require('../models');
+const { User, Thought, Reaction } = require('../models');
 connection.on('error', (err) => err);
 
 connection.once('open', async () => {
   console.log('connected');
-const users = [
-  {
-    username:'Amy',
-    email:'amy@gmail.com',
-  },{
-  username:'Tam',
-    email:'tam@gmail.com',
-    },{
-      username:'Bonie',
-      email:'bonie@gmail.com',
-    },{
-      username:'Fonda',
-      email:'fonda@gmail.com',
-    },{
-      username:'Vikki',
-      email:'vikki@gmail.com',
-    },{
-    username:'Tony',
-    email:'tony@gmail.com',
+  const users = [
+    {
+      username: 'Amy',
+      email: 'amy@gmail.com',
+    }, {
+      username: 'Tam',
+      email: 'tam@gmail.com',
+    }, {
+      username: 'Bonie',
+      email: 'bonie@gmail.com',
+    }, {
+      username: 'Fonda',
+      email: 'fonda@gmail.com',
+    }, {
+      username: 'Vikki',
+      email: 'vikki@gmail.com',
+    }, {
+      username: 'Tony',
+      email: 'tony@gmail.com',
+    },
+    {
+      username: 'Alice',
+      email: 'alice@gmail.com',
     }
   ];
   const thoughts = [
     {
-        thoughtText: "Have lots of work to finish",
-        username: "Tam"
+      thoughtText: "Today is the ..",
+      username: "Amy"
     },
     {
-        thoughtText: "Its a good app",
-        username: "Bonie"
+      thoughtText: "Have lots of work to finish",
+      username: "Tam"
+    },
+    {
+      thoughtText: "Its a good app",
+      username: "Bonie"
+    },
+    {
+      thoughtText: "Finsih Hw 18",
+      username: "Vikki"
+    },
+    {
+      thoughtText: "Have a event to go ..",
+      username: "Tony"
+    },
+
+    {
+      thoughtText: "Weather is great to ..",
+      username: "Alice"
     }
-];
-await User.collection.insertMany(users);
+  ];
+  
+  await User.collection.insertMany(users,{forceServerObjectId: false});
 
-// Add thoughts to the collection and await the results
-await Thought.collection.insertMany(thoughts);
+  // Add thoughts to the collection and await the results
+  await Thought.collection.insertMany(thoughts,{forceServerObjectId: false});
 
-// Log out the seed data to indicate what should appear in the database
-console.table(users);
-console.table(thoughts);
-console.info('Seeding complete! 🌱');
-process.exit(0);
-  });
+  // Log out the seed data to indicate what should appear in the database
+  console.table(users);
+  console.table(thoughts);
+  console.info('Seeding complete! 🌱');
+  process.exit(0);
+});
